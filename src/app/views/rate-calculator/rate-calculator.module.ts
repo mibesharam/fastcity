7d3d2ec04a6f43c948d817938282145cf8722b6a
mibesharam <k.bhavin.kb@gmail.com>
2020-07-05 02:30:36 +0530

    tracking done

--- src/app/views/rate-calculator/rate-calculator.module.ts
+++ src/app/views/rate-calculator/rate-calculator.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RateCalculatorComponent } from './rate-calculator.component';
+import { MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   declarations: [RateCalculatorComponent],
   exports: [RateCalculatorComponent]
--- src/app/views/track/track.module.ts
+++ src/app/views/track/track.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackComponent } from './track.component';
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [TrackComponent],
   exports: [TrackComponent]
